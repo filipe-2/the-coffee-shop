df1624dfa3e1f0e4fc45131c0b8ccc76f0649393
Carlos Filipe <carlos.filipe.ramos.12>
2024-02-13 16:17:26 -0300

    new scrolling control button and more functionality

--- JS/main-script.js
+++ JS/main-script.js
@@ -1,6 +1,32 @@
+window.addEventListener('scroll', function () {
+    let scroll = document.querySelectorAll('.scrollBtn');
+    let isOnBottom = (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
+    let scrollTop = scroll[0];
+    let scrollBottom = scroll[1];
+
+    scrollTop.classList.toggle('active', window.scrollY > 0);
+    scrollBottom.classList.toggle('active', !isOnBottom);
+
+    // Positioning the scrolling buttons based on the position inside the page 
+    if (this.window.scrollY <= 0 || isOnBottom) {
+        scrollTop.style.marginBottom = '-50px';
+    } else {
+        scrollTop.style.marginBottom = '0'
+    }
+})
+
+// Function to go back to the top of the page
 function goToTop() {
     window.scrollTo({
         top: 0,
         behavior: 'smooth',
     });
+}
+
+// Function to go back to the bottom of the page
+function goToBottom() {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth',
+    })
 }
