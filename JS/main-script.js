e5a88d26c4300f4634e4bc1ad8ac1fea11f1d72a
Carlos Filipe <carlos.filipe.ramos.12>
2024-02-24 18:14:13 -0300

    Small change

--- JS/main-script.js
+++ JS/main-script.js
@@ -1,11 +1,14 @@
 import {
+    // Functions
     startSlideshow,
     goToTop,
     goToBottom,
     toggleNavbarVisibility,
     toggleHamburgerMenu,
-    hamburger,
     handleScrollButtons,
+
+    // Variables
+    hamburger,
 } from './utils.js';
 
 window.addEventListener('scroll', toggleNavbarVisibility); // Toggles the navbar visibility when scrolling
@@ -13,8 +16,6 @@ hamburger.addEventListener('click', toggleHamburgerMenu); // Toggles the hamburg
 
 window.addEventListener('load', startSlideshow); // Starts the slideshow when the page loads
 
-window.addEventListener('scroll', handleScrollButtons) // Handles scroll button position and visibility
-
-// Adds click events to scroll buttons
-document.querySelector('.scrollBtn[data-action="top"]').addEventListener('click', goToTop);
-document.querySelector('.scrollBtn[data-action="bottom"]').addEventListener('click', goToBottom);
+window.addEventListener('scroll', handleScrollButtons) // Handles position and visibility of scroll buttons
+document.querySelector('.scrollBtn[data-action="top"]').addEventListener('click', goToTop); // Goes to the top of the page when clicking
+document.querySelector('.scrollBtn[data-action="bottom"]').addEventListener('click', goToBottom); // Goes to the bottom of the page when clicking
